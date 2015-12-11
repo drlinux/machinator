d99291f995285f2710ebbf5bc07f6313e72e5d8f
Ibrahim Yilmaz (drlinux) <ibrahim@zetaops.io>
2015-12-11 13:20:59 +0200

    just

--- reverse-proxy/build.sh
+++ reverse-proxy/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 clear
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
-if [[$( /usr/bin/docker rm cheri-cart)]]; then echo "Old container removed"; fi
+if [[$( docker rm nginx-reverse-proxy)]]; then echo "Old container removed"; fi
 echo "Building docker image... Please be patient... This may take some time..."
 
-/usr/bin/docker build -t machinator/nginx-reverse-proxy .
+docker build -t machinator/nginx-reverse-proxy .
 
 curl -o insecure_key -fSL https://github.com/phusion/baseimage-docker/raw/master/image/insecure_key
 
@@ -12,8 +12,8 @@ chmod 600 insecure_key
 
 echo "Runing the container"
 
-/usr/bin/docker run -d -i --name="machinator-reverse-proxy" -t machinator/nginx-reverse-proxy
+docker run -d -i --name="machinator-reverse-proxy" -t machinator/nginx-reverse-proxy
 
-/usr/bin/docker stop machinator-reverse-proxy
+docker stop machinator-reverse-proxy
 
 echo " ./run.sh"
